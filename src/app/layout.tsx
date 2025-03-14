4ddc0d9b6d623189057e8578c5960c7e879a6dc3
Oleksandr-Kalnaus <oleksandrkalnaus@gmail.com>
2025-03-14 21:48:14 +0000

    Added TailwindCSS, component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import "./globals.css";
+import React from 'react';
+import './globals.css';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body >
+        {children}
+      </body>
     </html>
-  );
+  )
 }
